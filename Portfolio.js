f0552f4c3f068138272ef5143573692345168670
omerkhan3 <okhan320@gmail.com>
2016-08-20 02:05:40 -0400

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('#item').mouseover(function(){
+		$('#item').css('background-color', '#C0C0C0');
+		
+		
+	});
+	
+	
+
+	$('#item').mouseout(function() {
+				$('#item').css('background-color', 'white');
+
+	});
+	
+});
